c6734f201d42e21c88c9d6c8b4569c7660116b54
Svetlana_Kovalevich <svetik14-06@mail.ru>
2023-06-05 23:22:35 +0300

    функциональная компонента Button

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from "classnames";
 
- class Button extends React.Component {
+ /*class Button extends React.Component {
     render() {
        // return <button className={"button button--outline"}>{this.props.text}</button>
        // return <button className={ `button ${this.props.outline ? "button--outline": ''}`}>{this.props.children}</button>
@@ -9,6 +9,16 @@ import classNames from "classnames";
             'button--outline':this.props.outline
         })}>{this.props.children}</button>
     }
+}*/
+
+function Button(props) {
+    return(
+        <button className={classNames('button',{
+            'button--outline':props.outline
+        })}>{props.children}</button>
+    )
+
 }
+
 export default Button
 
